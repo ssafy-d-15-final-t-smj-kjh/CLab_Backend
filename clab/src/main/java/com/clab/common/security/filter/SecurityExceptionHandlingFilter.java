d7e3e8228b3dfb273ded2107c7855b3ccb6fe3c6
jh010303 <jh010303@naver.com>
2026-06-13 04:55:34 +0900

    refactor: 커스텀필터 응답 통일
    - 커스텀필터에서 예외가 발생했을 때 apiResponse 사용

--- clab/src/main/java/com/clab/common/security/filter/SecurityExceptionHandlingFilter.java
+++ clab/src/main/java/com/clab/common/security/filter/SecurityExceptionHandlingFilter.java
@@ -9,11 +9,12 @@ import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.clab.common.exception.ApiResponse;
+import com.clab.common.exception.ErrorCode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.MalformedJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -33,8 +34,7 @@ public class SecurityExceptionHandlingFilter extends OncePerRequestFilter {
 			if (e instanceof ExpiredJwtException) {
 				log.debug("JWT 만료 예외 발생: {}, {}", request.getRequestURI(), e);
 				setErrorResponse(response, HttpStatus.UNAUTHORIZED, "TOKEN_EXPIRED");
-			}
-			else if (e instanceof JwtException) {
+			} else if (e instanceof JwtException) {
 				log.debug("JWT 예외 발생: {}, {}", request.getRequestURI(), e);
 				setErrorResponse(response, HttpStatus.UNAUTHORIZED, "TOKEN_INVALID");
 			} else if (e instanceof BadCredentialsException) {
@@ -47,11 +47,11 @@ public class SecurityExceptionHandlingFilter extends OncePerRequestFilter {
 
 	private void setErrorResponse(HttpServletResponse response, HttpStatus status, String message) throws IOException {
 		log.debug("커스텀 필터에서 발생한 예외 처리: {}, {}", status, message);
-		response.setStatus(status.value());
-		response.setContentType(MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8");
-		ObjectMapper objectMapper = new ObjectMapper();
-		Map<String, Object> errorDetails = Map.of("status", status.value(), "message", message);
 
-		response.getWriter().write(objectMapper.writeValueAsString(errorDetails));
+        ApiResponse<?> apiResponse = new ApiResponse<>(ErrorCode.UNAUTHORIZED, message);
+        response.setStatus(status.value());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE + ";charset=UTF-8");
+        ObjectMapper objectMapper = new ObjectMapper();
+        response.getWriter().write(objectMapper.writeValueAsString(apiResponse));
 	}
 }

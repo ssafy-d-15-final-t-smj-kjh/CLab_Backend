09732dc342385832e055cf7d0c52e33d32872b87
= <tjalswhd1@naver.com>
2026-06-12 14:09:53 +0900

    feat : ExceptionHandler 설정 - CustomException - MethodArgumentNotValidException - HttpMessageNotReadableException - AccessDeniedException - AuthenticationException - Exception

--- clab/src/main/java/com/clab/common/exception/ErrorCode.java
+++ clab/src/main/java/com/clab/common/exception/ErrorCode.java
@@ -7,44 +7,12 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode implements BaseCode {
 	// Common Error --- BEGIN
-	// 잘못된 서버 요청
-	BAD_REQUEST_ERROR(400, "G-001", "Bad Request Exception"),
-
-	// @RequestBody 데이터 미 존재
-	REQUEST_BODY_MISSING_ERROR(400, "G-002", "Required request body is missing"),
-
-	// 유효하지 않은 타입
-	INVALID_TYPE_VALUE(400, "G-003", " Invalid Type Value"),
-
-	// Request Parameter 로 데이터가 전달되지 않을 경우
-	MISSING_REQUEST_PARAMETER_ERROR(400, "G-004", "Missing Servlet RequestParameter Exception"),
-
-	// 입력/출력 값이 유효하지 않음
-	IO_ERROR(400, "G-005", "I/O Exception"),
-
-	// com.google.gson JSON 파싱 실패
-	JSON_PARSE_ERROR(400, "G-006", "JsonParseException"),
-
-	// com.fasterxml.jackson.core Processing Error
-	JACKSON_PROCESS_ERROR(400, "G-007", "com.fasterxml.jackson.core Exception"),
-
-	// 권한이 없음
-	FORBIDDEN_ERROR(403, "G-008", "Forbidden Exception"),
-
-	// 서버로 요청한 리소스가 존재하지 않음
-	NOT_FOUND_ERROR(404, "G-009", "Not Found Exception"),
-
-	// NULL Point Exception 발생
-	NULL_POINT_ERROR(404, "G-010", "Null Point Exception"),
-
-	// @RequestBody 및 @RequestParam, @PathVariable 값이 유효하지 않음
-	NOT_VALID_ERROR(404, "G-011", "handle Validation Exception"),
-
-	// @RequestBody 및 @RequestParam, @PathVariable 값이 유효하지 않음
-	NOT_VALID_HEADER_ERROR(404, "G-012", "Header에 데이터가 존재하지 않는 경우 "),
-
-	// 서버가 처리 할 방법을 모르는 경우 발생
-	INTERNAL_SERVER_ERROR(500, "G-999", "Internal Server Error Exception"),
+	INVALID_REQUEST(400, "ERR-REQ-400", "잘못된 요청입니다."),
+    INVALID_JSON(400, "ERR-JSON-400", "요청 본문 형식이 올바르지 않습니다."),
+    UNAUTHORIZED(401, "ERR-AUTH-401", "인증이 필요합니다."),
+    FORBIDDEN(403, "ERR-FORBIDDEN-403", "접근 권한이 없습니다."),
+    NOT_FOUND(404, "ERR-NOTFOUND-404", "대상을 찾을 수 없습니다."),
+    INTERNAL_SERVER_ERROR(500, "ERR-SERVER-500", "서버 내부 오류가 발생했습니다."),
 	// Common Error --- END
 
 	

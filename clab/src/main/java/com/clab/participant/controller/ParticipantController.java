c06ece0b0a19b082e99c1e31022dff815b8dba50
= <tjalswhd1@naver.com>
2026-06-14 13:46:18 +0900

    fix : 어노테이션 잘못된 import 수정

--- clab/src/main/java/com/clab/participant/controller/ParticipantController.java
+++ clab/src/main/java/com/clab/participant/controller/ParticipantController.java
@@ -6,9 +6,9 @@ import java.util.Map;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +18,6 @@ import com.clab.participant.dto.ParticipantDto;
 import com.clab.participant.service.ParticipantService;
 
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
